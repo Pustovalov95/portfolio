c38552cc11e86224242df13fe5b42c8fb4c469c7
Dmitry <dmitrypustovalov95@gmail.com>
2021-12-15 14:14:08 +0300

    renamed arrows

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
       autoplay: true,
       autoplaySpeed: 2000,
       arrows: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="../img/examples/arrow-l.png" alt="prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="../img/examples/arrow-r.png" alt="next"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="../img/examples/arrow_l.png" alt="prev"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../img/examples/arrow_r.png" alt="next"></button>',
       responsive: [
         {
           breakpoint: 767,
